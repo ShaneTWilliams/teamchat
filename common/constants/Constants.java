c9367e76328cfaf6d0105ca238e5c4215b516ab8
Shane Williams <shanethomaswilliams@gmail.com>
2022-02-27 16:56:49 -0330

    Add project constants file

---
+++ common/constants/Constants.java
@@ -0,0 +1,10 @@
+package common.constants;
+
+public class Constants {
+    public static final int SERVER_PORT = 6789;
+    public static final String SERVER_LOCAL_IP = "0.0.0.0";
+    public static final int USERNAME_MAX_LEN = 50;
+    public static final int USERNAME_MIN_LEN = 5;
+    public static final int MESSAGE_MAX_LEN = 1000;
+    public static final int MESSAGE_MIN_LEN = 1;
+}
